58913893072974367833d05c4a3bac48f7847c54
UdsonAvila <udsonmalaquias@gmail.com>
2022-09-19 08:26:07 -0300

    ajustando layouts home

--- resources/views/app/lista_compras.blade.php
+++ resources/views/app/lista_compras.blade.php
@@ -1,34 +1,40 @@
 @extends('layouts.app')
 @section('content')
+<div class="container">
 <h3>Layout Lista de Compras</h3>
-<table class="table table-hover">
+<div class="table-responsive">
+<table class="table table-hover"  aria-describedby="mydesc">
   <thead>
     <tr>
-      <th scope="col">Nº</th>
+      <th scope="col" style="width: 5%;">Itens</th>
+      <th scope="col"  style="width: 10%;">No Carrinho?</th>
+      <th scope="col" style="width: 15%;">Quantidade</th>
       <th scope="col">Produto</th>
-      <th scope="col">Quantidade</th>
-      <th scope="col">No Carrinho?</th>
     </tr>
   </thead>
   <tbody>
     <tr>
       <th scope="row">1</th>
-      <td>Café</td>
-      <td>2</td>
       <td>sim</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Arroz</td>
       <td>2</td>
-      <td>não</td>
+      <td>Café</td>
     </tr>
+   
     <tr>
     <th scope="row">2</th>
-      <td>feijão</td>
-      <td>5</td>
       <td>não</td>
+      <td>5</td>
+      <td>feijão</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>não</td>
+      <td>2</td>
+      
+      <td>Arroz</td>
     </tr>
   </tbody>
 </table>
+</div>
+</div>
 @endsection
--- resources/views/app/provisoes.blade.php
+++ resources/views/app/provisoes.blade.php
@@ -1,1 +1,34 @@
-PROVISOES
+
+@extends('layouts.app')
+@section('content')
+<div class="container">
+  
+<h3>Layout Provisões</h3>
+  <form>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Nome do Produto</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+
+    </div>
+    <br>
+    <div class="row">
+      <div class="form-group col-4">
+        <label for="exampleInputPassword1">Quantidade Minima</label>
+        <input type="number" class="form-control" id="exampleInputPassword1">
+      </div>
+      <div class="form-group  col-4">
+        <label for="exampleInputPassword1">Quantidade Maxima</label>
+        <input type="number" class="form-control" id="exampleInputPassword1">
+      </div>
+      <div class="form-group  col-4">
+        <label for="exampleInputPassword1">Quantidade Estocada</label>
+        <input type="number" class="form-control" id="exampleInputPassword1">
+      </div>
+    </div>
+    <br>
+    <button type="submit" class="btn btn-primary">Cadastrar</button>
+  </form>
+</div>
+
+
+@endsection
